ef251149aa274522c3a0cc91667e23662d84c364
OHYA Satoshi <conatus.11099@gmail.com>
2023-12-12 19:14:11 +0900

    データ型をテンプレートにした

--- Quaternion.h
+++ Quaternion.h
@@ -11,24 +11,57 @@
 #include <array>
 #include <cmath>
 
+
+
+template<typename T=float>
 class Quaternion {
 public:
 	Quaternion(){}
-	Quaternion(std::array<float, 3> vector3){
+	Quaternion(std::array<T, 3> vector3){
 		for(uint8_t n=1; n<4; n++){
 			value[n] = vector3[n];
 		}
 		value[0] = 0;
 	}
-	Quaternion(float val0, float val1, float val2, float val3){
+	Quaternion(std::array<T, 4> vector4){
+		for(uint8_t n=1; n<4; n++){
+			value[n] = vector4[n];
+		}
+		value[0] = 0;
+	}
+	Quaternion(T val0, T val1, T val2, T val3){
 		value[0] = val0;
 		value[1] = val1;
 		value[2] = val2;
 		value[3] = val3;
 	}
 
+	std::array<std::array<T, 3>, 3> rotationMat(){
+		std::array<std::array<T, 3>, 3> tmp;
+		tmp[0][0] = 2*std::pow(value[0],2)+2*std::pow(value[1],2)-1;
+		tmp[0][1] = 2*(value[1]*value[2]-value[3]*value[0]);
+		tmp[0][2] = 2*(value[1]*value[3]+value[2]*value[0]);
+		tmp[1][0] = 2*(value[1]*value[2]+value[3]*value[0]);
+		tmp[1][1] = 2*std::pow(value[0],2)+2*std::pow(value[2],2)-1;
+		tmp[1][2] = 2*(value[2]*value[3]-value[1]*value[0]);
+		tmp[2][0] = 2*(value[1]*value[3]-value[2]*value[0]);
+		tmp[2][1] = 2*(value[2]*value[3]+value[2]*value[0]);
+		tmp[2][2] = 2*std::pow(value[0],2)+2*std::pow(value[3],2)-1;
+
+		return tmp;
+	}
+
+	std::array<T, 3> rotateVector(std::array<T, 3> arg){
+		const std::array<std::array<T, 3>, 3> rotationMat = this->rotationMat();
+		std::array<T, 3> tmp;
+		tmp[0] = rotationMat[0][0]*arg[0] + rotationMat[0][1]*arg[1] + rotationMat[0][2]*arg[2];
+		tmp[1] = rotationMat[1][0]*arg[0] + rotationMat[1][1]*arg[1] + rotationMat[1][2]*arg[2];
+		tmp[2] = rotationMat[2][0]*arg[0] + rotationMat[2][1]*arg[1] + rotationMat[2][2]*arg[2];
+		return tmp;
+	}
+
 	Quaternion normalize(){
-		float size = 0;
+		T size = 0;
 		for(auto &it:value){
 			size += std::pow(it,2);
 		}
@@ -42,6 +75,10 @@ public:
 			it /= size;
 		}
 		return *this;
+	}
+
+	Quaternion invers(){
+		return Quaternion(value[0],value[1],value[2],value[3]);
 	}
 
 	Quaternion operator + (Quaternion &obj){
@@ -68,7 +105,7 @@ public:
 				value[0]*obj[3]+value[1]*obj[2]-value[2]*obj[1]+value[3]*obj[0]);
 	}
 
-	Quaternion operator*(float val){
+	Quaternion operator*(T val){
         Quaternion tmp;
 		for(uint8_t n=0; n<4; n++){
 			tmp[n] = value[n] * val;
@@ -92,7 +129,7 @@ public:
 		return tmp;
 	}
 
-	Quaternion operator/(float val){
+	Quaternion operator/(T val){
         Quaternion tmp;
 		for(uint8_t n=0; n<4; n++){
 			tmp[n] = value[n] / val;
@@ -100,15 +137,26 @@ public:
 		return tmp;
 	}
 
-	void operator*=(float val){
+	Quaternion operator*=(T val){
 		for(auto &it:value){
 			it *= val;
 		}
+		return *this;
 	}
 
-	float & operator [](int n) { return value[n]; }
+	Quaternion operator*=(Quaternion obj){
+		Quaternion tmp(
+			value[0]*obj[0]-value[1]*obj[1]-value[2]*obj[2]-value[3]*obj[3],
+			value[0]*obj[1]-value[1]* obj[0]+value[2]*obj[3]-value[3]*obj[2],
+			value[0]*obj[2]-value[1]*obj[3]+value[2]*obj[0]+value[3]*obj[1],
+			value[0]*obj[3]+value[1]*obj[2]-value[2]*obj[1]+value[3]*obj[0]);
+
+		return *this = tmp;
+	}
+
+	T & operator [](int n) { return value[n]; }
 private:
-	std::array<float, 4> value;
+	std::array<T, 4> value;
 };
 
 #endif /* QUATERNION_QUATERNION_H_ */
